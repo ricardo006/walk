1bc98fea6d3a82ae2148c4c875c0bb879552d7a0
Ricardo <rycardo.191@gmail.com>
2024-01-17 15:58:04 -0300

    first commit

---
+++ dist/test/integration/opaTests.qunit.js
@@ -0,0 +1,2 @@
+QUnit.config.autostart=false;sap.ui.getCore().attachInit(()=>{"use strict";sap.ui.require(["ui5/walkthrough/localService/mockserver","ui5/walkthrough/test/integration/NavigationJourney"],t=>{t.init();QUnit.start()})});
+//# sourceMappingURL=opaTests.qunit.js.map
