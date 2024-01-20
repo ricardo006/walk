8435f69feaf5e31f3a1c7d24f8fbf180ff8131b1
Ricardo <rycardo.191@gmail.com>
2024-01-20 12:41:56 -0300

    Ajustes projeto

---
+++ dist/test/unit/unitTests.qunit.js
@@ -0,0 +1,2 @@
+QUnit.config.autostart=false;sap.ui.getCore().attachInit(()=>{"use strict";sap.ui.require(["ui5/walkthrough/test/unit/model/formatter"],()=>QUnit.start())});
+//# sourceMappingURL=unitTests.qunit.js.map
